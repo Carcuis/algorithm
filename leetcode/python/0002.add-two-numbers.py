d2273c24ed26abe9e62fe8849ab8269121b3d42c
Carcuis <ctx1256@foxmail.com>
2025-01-12 12:53:35 +0800

    leet: update solution for problem 2

--- leetcode/python/0002.add-two-numbers.py
+++ leetcode/python/0002.add-two-numbers.py
@@ -17,23 +17,14 @@ class ListNode:
 class Solution:
     def addTwoNumbers(self, l1: ListNode | None, l2: ListNode | None) -> ListNode | None:
         def _add(l: ListNode | None, r: ListNode | None, carry: int) -> tuple[ListNode | None, int]:
-            if l is None:
-                if r is None:
+            if l is None or r is None:
+                if l is None:
+                    l, r = r, l
+                if l is None:
                     return (ListNode(1), 0) if carry == 1 else (None, 0)
-                _carry = (r.val + carry) // 10
-                r.val = (r.val + carry) % 10
-                r.next, carry = _add(None, r.next, _carry)
-                return r, carry
 
-            if r is None:
-                _carry = (l.val + carry) // 10
-                l.val = (l.val + carry) % 10
-                l.next, carry = _add(None, l.next, _carry)
-                return l, carry
-
-            _carry = (l.val + r.val + carry) // 10
-            l.val = (l.val + r.val + carry) % 10
-            l.next, carry = _add(l.next, r.next, _carry)
+            carry, l.val = divmod(l.val + (r.val if r else 0) + carry, 10)
+            l.next, carry = _add(l.next, r.next if r else None, carry)
             return l, carry
 
         return _add(l1, l2, 0)[0]
@@ -41,7 +32,7 @@ class Solution:
 
 
 class Test(unittest.TestCase):
-    solution = Solution()
+    add = Solution().addTwoNumbers
 
     def list_to_array(self, node: ListNode | None) -> list[int]:
         array = []
@@ -63,20 +54,22 @@ class Test(unittest.TestCase):
     def test_1(self):
         l1 = self.array_to_list([2, 4, 3])
         l2 = self.array_to_list([5, 6, 4])
-        result = self.solution.addTwoNumbers(l1, l2)
-        self.assertEqual(self.list_to_array(result), [7, 0, 8])
+        self.assertEqual(self.list_to_array(self.add(l1, l2)), [7, 0, 8])
 
     def test_2(self):
         l1 = self.array_to_list([0])
         l2 = self.array_to_list([0])
-        result = self.solution.addTwoNumbers(l1, l2)
-        self.assertEqual(self.list_to_array(result), [0])
+        self.assertEqual(self.list_to_array(self.add(l1, l2)), [0])
 
     def test_3(self):
         l1 = self.array_to_list([9, 9, 9, 9, 9, 9, 9])
         l2 = self.array_to_list([9, 9, 9, 9])
-        result = self.solution.addTwoNumbers(l1, l2)
-        self.assertEqual(self.list_to_array(result), [8, 9, 9, 9, 0, 0, 0, 1])
+        self.assertEqual(self.list_to_array(self.add(l1, l2)), [8, 9, 9, 9, 0, 0, 0, 1])
+
+    def test_4(self):
+        l1 = self.array_to_list([2, 4, 9])
+        l2 = self.array_to_list([5, 6, 4, 9])
+        self.assertEqual(self.list_to_array(self.add(l1, l2)), [7, 0, 4, 0, 1])
 
 
 if __name__ == '__main__':
